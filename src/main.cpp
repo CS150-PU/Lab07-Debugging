d054757eacc9dd78a91fbb4a1d57bd9527cdbe07
ShereenKhoja <43831078+ShereenKhoja@users.noreply.github.com>
2023-10-10 14:58:19 +0000

    Wrote the code

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,48 @@
 //******************************************************************************
 
 #include <iostream>
+#include <fstream>
+#include <string>
 
 using namespace std;
 
 int main () {
+  const string INPUT_FILE = "data/numbers.txt";
+	const int SENTINEL = -999;
+	const bool SORTED = true,
+		        NOT_SORTED = false;
+
+	int currentNumber = 0,
+		  previousNumber;
+	bool bSorted = SORTED;
+
+	ifstream inputFile;
+	inputFile.open (INPUT_FILE);
+
+	if (inputFile.fail ()) {
+		cout << "Error opening input file." << endl;
+		exit (EXIT_FAILURE);
+	}
+
+	cout << "*** Number Cruncher ***" << endl << endl;
+
+	while (currentNumber != SENTINEL) {
+		previousNumber = currentNumber;
+		inputFile >> currentNumber;
+		if (previousNumber > currentNumber);
+		bSorted = NOT_SORTED;
+	}
+
+	if (bSorted = SORTED) {
+		cout << "SORTED" << endl;
+	}
+	else {
+		cout << "NOT SORTED" << endl;
+	}
+
+  cout << "Reached the end" << endl << endl;
+
+	inputFile.close ();
 
   return EXIT_SUCCESS;
 }
